c2053dad0a74413aa8eaaf1056f6eab93bbab646
asya1098 <arsenijmozgov487@gmail.com>
2025-06-03 22:01:43 +0300

    v 0.1.2

---
+++ src/Menu.php
@@ -0,0 +1,26 @@
+<?php
+class Menu{
+    public $data;
+    public function __construct() {
+        $this->data = new \src\receivingData();
+    }
+    public function showMenu()
+{
+    return $this->data->format(readline("\nВыберите вариант:\n
+    1. Погода сейчас\n
+    2. Погода на следующие 3 дня\n
+    3. Выход\n
+    Ваш выбор: "));
+}
+    public function choiceMenu(){
+        $choice = $this->showMenu();
+        switch($choice){
+            case 1:
+                $this->data->setFormatUrl("?format=%c%t");
+            break;
+            case 2:
+                $this->data->setFormatUrl(null);
+            break;
+        }
+    }
+}
--- src/receivingData.php
+++ src/receivingData.php
@@ -3,20 +3,27 @@ namespace src;
 
 class ReceivingData{
 protected $url;
+protected $formatUrl;
 protected $city;
-public function format($str){
+public function format($str):string {
     return trim(strtolower($str));
 }
-public function setCity($city){
+public function setCity($city):void {
     $this->city = $city;
 }
-public function getCity(): mixed{
+public function getCity():string {
     return $this->city;
 }
-public function setUrl($city){
-    $this->url ="https://wttr.in/" . $city . "?format=%c%t";
+public function setUrl($city):void {
+    $this->url = "https://wttr.in/" . $city . "?format=%c%t";
 }
-public function getUrl(){
+public function getUrl():string {
     return $this->url;
+}
+public function setFormatUrl($formatUrl):void {
+    $this->formatUrl = $formatUrl;
+}
+public function getFormatUrl():string {
+    return $this->formatUrl;
 }
 }
